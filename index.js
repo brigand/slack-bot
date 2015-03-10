e3f7c5c683110db02a525830b876feaabeeb91b5
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-03-10 14:44:57 -0700

    initial

---
+++ index.js
@@ -0,0 +1,53 @@
+const irc = require('irc');
+const superagent = require('superagent');
+
+const config = {
+    slack: {
+        token: process.env.SLACK_TOKEN,
+        channel: '#' + process.env.SLACK_CHANNEL
+    }
+};
+
+console.log('config', config);
+
+const cat_url = 'http://thecatapi.com/api/images/get?format=src';
+sendRandomImageUrl();
+setInterval(sendRandomImageUrl, 1000*60*30);
+
+function getCatImage(cb){
+    superagent.head(cat_url)
+        .end(function(res){
+            console.log('GET', res.statusCode, cat_url);
+            if (res.statusCode !== 302) {
+                return cb(res);
+            }
+            console.log(res.headers);
+            cb(null, res.headers.location);
+        });
+}
+
+function sendRandomImageUrl(){
+    getCatImage((err, url) => {
+        sendToSlack('KittenLord437', url, '#random', (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+}
+
+function sendToSlack(username, text, channel, cb){
+    superagent.post("https://slack.com/api/chat.postMessage").type("form").send({
+        token: config.slack.token,
+        channel: channel || config.slack.channel,
+        text: text,
+        username: username
+   }).end(function (res) {
+        console.log("POST", res.statusCode, "https://slack.com/api/chat.postMessage");
+        if (res.statusCode >= 300) {
+             cb(res);
+        }
+        cb(null);
+   });
+}
+
---
+++ run.sh
@@ -0,0 +1,5 @@
+babel index.js > bot.js
+
+SLACK_TOKEN=xoxb-3986284148-gcBbbNovqqZLVf3FfYHRiasp \
+SLACK_CHANNEL=random \
+node bot.js
